ad731ce7b4ed16bd3e7299e308993d2733dec1f3
viettu2k <doanviettu2k@gmail.com>
2022-08-01 10:27:22 +0700

    fix ngx loader and implement register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { GlobalConstants } from './../shared/global-constants';
+import { SnackbarService } from './../services/snackbar/snackbar.service';
+import { UserService } from './../services/user/user.service';
+import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
+  registerForm: any = FormGroup;
+  responseMessage: string = '';
+  loginSub!: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService,
+    private snackbarService: SnackbarService,
+    private dialogRef: MatDialogRef<RegisterComponent>,
+    private ngxService: NgxUiLoaderService
+  ) {}
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
   }
 
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: [
+        null,
+        [Validators.required, Validators.pattern(GlobalConstants.nameRegex)],
+      ],
+      email: [
+        null,
+        [Validators.required, Validators.pattern(GlobalConstants.emailRegex)],
+      ],
+      contact: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern(GlobalConstants.contactNumberRegex),
+        ],
+      ],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  handleSubmit() {
+    this.ngxService.start();
+    const { name, email, contact, password } = this.registerForm.value;
+    this.loginSub = this.userService
+      .register({ name, email, contact, password })
+      .subscribe(
+        (response: any) => {
+          this.ngxService.stop();
+          this.dialogRef.close();
+          this.responseMessage = response?.message;
+          this.snackbarService.openSnackBar(this.responseMessage, '');
+          this.router.navigate(['/']);
+        },
+        (error) => {
+          this.ngxService.stop();
+          if (error.error?.message) {
+            this.responseMessage = error.error.message;
+          } else {
+            this.responseMessage = GlobalConstants.genericError;
+          }
+          this.snackbarService.openSnackBar(
+            this.responseMessage,
+            GlobalConstants.error
+          );
+        }
+      );
+  }
+
+  OnDestroyy(): void {
+    if (this.loginSub) {
+      this.loginSub.unsubscribe();
+    }
+  }
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from './../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  url = environment.apiURL;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  register(data: any) {
+    return this.httpClient.post(`${this.url}/user/register`, data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json'),
+    });
+  }
 }
