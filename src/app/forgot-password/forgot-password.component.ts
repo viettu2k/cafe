ab0c9c8881a450130c939cac5a24ef368611380d
viettu2k <doanviettu2k@gmail.com>
2022-08-04 10:07:43 +0700

    updating forgot-password component

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,15 +1,59 @@
+import { GlobalConstants } from './../shared/global-constants';
+import { SnackbarService } from './../services/snackbar/snackbar.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { MatDialogRef } from '@angular/material/dialog';
+import { UserService } from './../services/user/user.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent implements OnInit {
+  forgotPasswordForm: any = FormGroup;
+  responseMessage: any;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private dialogRef: MatDialogRef<ForgotPasswordComponent>,
+    private ngxService: NgxUiLoaderService,
+    private snackbarService: SnackbarService
+  ) {}
 
   ngOnInit(): void {
+    this.forgotPasswordForm = this.formBuilder.group({
+      email: [
+        null,
+        [Validators.required, Validators.pattern(GlobalConstants.emailRegex)],
+      ],
+    });
   }
 
+  handleSubmit() {
+    this.ngxService.start();
+    const { email } = this.forgotPasswordForm.value;
+    this.userService.forgotPassword({ email }).subscribe(
+      (response: any) => {
+        this.ngxService.stop();
+        this.responseMessage = response?.message;
+        this.dialogRef.close();
+        this.snackbarService.openSnackBar(this.responseMessage, '');
+      },
+      (error) => {
+        this.ngxService.stop();
+        if (error.error?.message) {
+          this.responseMessage = error.error.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackbarService.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
+      }
+    );
+  }
 }
