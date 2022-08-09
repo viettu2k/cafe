92348dcd6080c9b886f7f4c3e8a680033d17cb83
viettu2k <doanviettu2k@gmail.com>
2022-08-09 10:52:50 +0700

    adding logout change password c

--- src/app/layouts/full/header/header.component.ts
+++ src/app/layouts/full/header/header.component.ts
@@ -1,3 +1,5 @@
+import { ChangePasswordComponent } from './../../../material-component/dialog/change-password/change-password.component';
+import { ConfirmationComponent } from './../../../material-component/dialog/confirmation/confirmation.component';
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Router } from '@angular/router';
@@ -5,13 +7,30 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 export class AppHeaderComponent {
   role: any;
-  constructor(private router: Router,
-    private dialog: MatDialog) {
+  constructor(private router: Router, private dialog: MatDialog) {}
 
+  logout() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      message: 'Logout',
+    };
+    const dialogRef = this.dialog.open(ConfirmationComponent, dialogConfig);
+    const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe(
+      (user) => {
+        dialogRef.close();
+        localStorage.clear();
+        this.router.navigate(['/']);
+      }
+    );
   }
 
+  changePassword() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '550px';
+    this.dialog.open(ChangePasswordComponent, dialogConfig);
+  }
 }
