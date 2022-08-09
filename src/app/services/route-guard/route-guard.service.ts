772744faca262d51d73776beedf8bc20c9c47045
viettu2k <doanviettu2k@gmail.com>
2022-08-09 09:23:31 +0700

    adding route guard service

---
+++ src/app/services/route-guard/route-guard.service.ts
@@ -0,0 +1,53 @@
+import { GlobalConstants } from './../../shared/global-constants';
+import { SnackbarService } from './../snackbar/snackbar.service';
+import { ActivatedRouteSnapshot, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import jwt_decode from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RouteGuardService {
+  constructor(
+    public auth: AuthService,
+    public router: Router,
+    private snackbarService: SnackbarService
+  ) {}
+
+  canActivate(router: ActivatedRouteSnapshot): boolean {
+    let expectedRoleArray = router.data.expectedRole;
+
+    const token: any = localStorage.getItem('token');
+    var tokenPayload: any;
+    try {
+      tokenPayload = jwt_decode(token);
+    } catch (error) {
+      localStorage.clear();
+      this.router.navigate(['/']);
+    }
+
+    let checkRole = false;
+    for (const element of expectedRoleArray) {
+      if (tokenPayload.role === element) {
+        checkRole = true;
+      }
+    }
+
+    if (tokenPayload.role === 'user' || tokenPayload.role === 'admin') {
+      if (this.auth.isAuthenticated() && checkRole) {
+        return true;
+      }
+      this.snackbarService.openSnackBar(
+        GlobalConstants.unauthorizated,
+        GlobalConstants.error
+      );
+      this.router.navigate(['/cafe/dashboard']);
+      return false;
+    } else {
+      this.router.navigate(['/']);
+      localStorage.clear();
+      return false;
+    }
+  }
+}
